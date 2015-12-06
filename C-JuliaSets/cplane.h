eae10e1ca301377a11a69c7991c5f4aa1f5b8b02
Georgy Mokshantsev <moksh100@mail.chapman.edu>
2015-12-05 21:25:27 -0800

    add folders

--- C-JuliaSets/cplane.h
+++ C-JuliaSets/cplane.h
@@ -1,1 +1,2 @@
 typedef struct cplane  { long double xmin, xmax, ymin, ymax; unsigned long int xpoints, ypoints} CPLANE;
+CPLANE* CPLANE_new(long double xmin, xmax, ymin, ymax; unsigned long int xpoints, ypoints);
---
+++ C-JuliaSets/cplane.h~
@@ -0,0 +1,1 @@
+typedef struct cplane  { long double xmin, xmax, ymin, ymax; unsigned long int xpoints, ypoints} CPLANE;
