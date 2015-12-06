0403f64ec5d39d01cfc29e15fda3e9c7ae194d6f
Georgy Mokshantsev <moksh100@mail.chapman.edu>
2015-12-05 21:15:43 -0800

    stage 1 initial

---
+++ C-JuliaSets/complex.h
@@ -0,0 +1,7 @@
+typedef struct complexnumber{ int x; int y; } ComplexNumber;
+
+ComplexNumber mult2(ComplexNumber a,ComplexNumber b);
+ComplexNumber square(ComplexNumber a);
+ComplexNumber add2(ComplexNumber a, ComplexNumber b);
+ComplexNumber juliamap(ComplexNumber z, ComplexNumber c);
+char ** complex_to_str(ComplexNumber z); 
