cddd7bc1548ed86c12edf8300d8ff55071eabde4
Georgy Mokshantsev <moksh100@mail.chapman.edu>
2015-12-05 22:07:28 -0800

    add constructor to cplane

--- C-JuliaSets/cplane.c
+++ C-JuliaSets/cplane.c
@@ -1,4 +1,7 @@
 #include "cplane.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include "complex.h"
 
 CPLANE* CPLANE_new(long double xmin, xmax, ymin, ymax; unsigned long int xpoints, ypoints){
 	CPLANE* p = malloc(sizeof(CPLANE));
@@ -8,6 +11,24 @@ CPLANE* CPLANE_new(long double xmin, xmax, ymin, ymax; unsigned long int xpoints
 	p->ymax=ymax;
 	p->xpoints = xpoints;
 	p->ypoints=ypoints;
+	long double stepx = (xmax-xmin)/xpoints;
+	long double stepy = (ymax-ymin)/ypoints;
+	long double x;
+	long double y;
+	ComplexNumber mat[ypoints][xpoints];
+	int i;
+	i=0;
+	for (x=xmin; x<=xmax; x=x+stepx){
+		int j;
+		j=0;
+		for (y=ymin; y<=ymax; y=y+stepy){
+			mat[j][i].x= x;
+			mat[j][i].y=y;
+			j=j+1;
+		}
+		i=i+1;
+	}
 
+	p->mat=mat;
 	return p;
 }
--- C-JuliaSets/cplane.h
+++ C-JuliaSets/cplane.h
@@ -1,2 +1,4 @@
-typedef struct cplane  { long double xmin, xmax, ymin, ymax; unsigned long int xpoints, ypoints} CPLANE;
+#include "complex.h"
+
+typedef struct cplane  { long double xmin, xmax, ymin, ymax; unsigned long int xpoints, ypoints, ComplexNumber *mat} CPLANE;
 CPLANE* CPLANE_new(long double xmin, xmax, ymin, ymax; unsigned long int xpoints, ypoints);
