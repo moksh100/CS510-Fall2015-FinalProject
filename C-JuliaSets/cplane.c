ebafcbd3386347736547adf9f29cf1bb30e3bb63
moksh100 <moksh100@mail.chapman.edu>
2015-12-08 21:55:35 -0800

    add fixes to julia

--- C-JuliaSets/cplane.c
+++ C-JuliaSets/cplane.c
@@ -4,13 +4,13 @@
 
 
 CPLANE* CPLANE_new(long double xmin, long double xmax, long double ymin, long double ymax, unsigned long int xpoints, unsigned long int  ypoints){
-	CPLANE* p = malloc(sizeof(CPLANE));
-	p->xmin=xmin;
-	p->xmax=xmax;
-	p->ymin = ymin;
-	p->ymax=ymax;
-	p->xpoints = xpoints;
-	p->ypoints=ypoints;
+	CPLANE p;
+	p.xmin=xmin;
+	p.xmax=xmax;
+	p.ymin = ymin;
+	p.ymax=ymax;
+	p.xpoints = xpoints;
+	p.ypoints=ypoints;
 	long double stepx = (xmax-xmin)/xpoints;
 	long double stepy = (ymax-ymin)/ypoints;
 	long double x;
@@ -19,16 +19,22 @@ CPLANE* CPLANE_new(long double xmin, long double xmax, long double ymin, long do
 	int i;
 	int j;
 	i=0;
-	for (x=xmin; x<=xmax; x=x+stepx){
+	printf (" enter loop %d \n",i);
+	for (x=xmin; i<xpoints; x=x+stepx){
 		j=0;
-		for (y=ymin; y<=ymax; y=y+stepy){
-			mat[j][i].x= x;
-			mat[j][i].y= y;
+		for (y=ymin; j<ypoints; y=y+stepy){
+			ComplexNumber temp;
+			temp.x= x;
+			temp.y= y;
+			mat[j][i] = temp;
 			j=j+1;
+			printf ("loop j %d \n",j); 
 		}
 		i=i+1;
+	 printf ("loop i %d \n",i);
 	}
-
-	p->mat= mat;
-	return p;
+	printf ("assigning mat \n");
+	p.mat= &mat;
+	printf ("assigned mat \n");
+	return &p;
 }
