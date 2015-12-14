5aa9f3ac6f53f0952525724105977ac121d80a86
Georgy Mokshantsev <moksh100@mail.chapman.edu>
2015-12-13 23:01:07 -0800

    add python

---
+++ plot_data.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+
+import sys
+import pandas as pd
+
+for file in sys.argv:
+	if "csv" not in file:
+		continue
+	print "load data from "+file
+	df = pd.DataFrame.from_csv(file, index_col=None)
+	df.columns=['x', 'y' , 'one']
+	df1= df[['x' , 'y']]
+	ax = pd.DataFrame.plot(df1,x="x",y="y")
+	fig = ax.get_figure()
+	fig.savefig(file+'.jpg')
+
